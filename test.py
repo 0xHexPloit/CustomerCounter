5fadb23d830cec60cd4f972340fbe1df3ab5b908
0xHexPloit <78966024+0xHexPloit@users.noreply.github.com>
2023-06-05 00:00:01 +0200

    Removing unecessary file

--- test.py
+++
@@ -1,7 +0,0 @@
-from customercounter.electronic_device.electronic_device import ElectronicDevice
-from customercounter.event.type import EventType
-
-device = ElectronicDevice(device_id="AE-ED-32", vendor="Apple")
-device.update_device_state(EventType.PROBE_REQUEST_RECEIVED)
-device.update_device_state(EventType.NO_PROBE_REQUEST_RECEIVED)
-
