84004ae95eefb54d674be4129b9d2658c7578d7c
0xHexPloit <78966024+0xHexPloit@users.noreply.github.com>
2023-06-04 23:54:09 +0200

    Adding Event Receiver

---
+++ tests/event/test_event_receiver.py
@@ -0,0 +1,15 @@
+from multiprocessing import Pipe
+
+from customercounter.event.event import Event
+from customercounter.event.receiver import PipeEventReceiver
+
+
+def test_should_get_events_from_pipe(event: Event):
+    rx, tx = Pipe(duplex=False)
+    event_receiver = PipeEventReceiver(rx=rx)
+    tx.send([event])
+    received_events = event_receiver.get_events()
+
+    assert len(received_events) == 1
+    assert received_events[0].device_id == event.device_id
+    assert received_events[0].vendor == event.vendor
